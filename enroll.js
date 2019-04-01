b52d0a4f47224659209c765ce424a75d9f3c546b
hekun24 <1533431406@qq.com>
2019-04-01 23:30:10 +0800

    second updata

--- enroll.js
+++
@@ -1,7 +0,0 @@
-var enroll=document.getElementById("enroll");
-enroll.onclick=function(e){
-	e.preventDefault();
-	enroll.innerHTML="SUCCESS";
-	enroll.style.background="#27cb8b";
-	enroll.style.borderColor="#000000";
-}
